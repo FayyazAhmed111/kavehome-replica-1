ab37abd0745d509cc2a067c7e5424bc45e407e94
Fayyaz Ahmed <riskygaming330@gmail.com>
2025-10-23 20:42:59 +0500

    added singleproduct page

--- src/app/(pages)/singleproduct/[id]/page.jsx
+++ src/app/(pages)/singleproduct/[id]/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-export default function ProductsPage() {
+export default function SingleProductPage() {
   
 
   return (
-    <div>Products Page</div>
+    <div>Single Product Page</div>
   );
 }
