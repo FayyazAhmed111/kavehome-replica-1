0ba3e3c4e6dc1894be453bf113c2b7e345d6aafc
Fayyaz Ahmed <riskygaming330@gmail.com>
2025-11-10 04:10:48 +0500

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_WP_GRAPHQL_URL: 'https://sienna-raccoon-358091.hostingersite.com/graphql',
-  },
-  experimental: {
-    optimizeCss: false
-   },
+  }
 };
 
 export default nextConfig;
